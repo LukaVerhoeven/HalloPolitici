213e28c4ee710e2f87f3a58abd379937357f7235
DriesH <dries.heyninck@gmail.com>
2016-12-09 12:11:53 +0100

    added facebook login and api call post

--- MobileAPP/hallopolitici/www/js/index.js
+++ MobileAPP/hallopolitici/www/js/index.js
@@ -21,30 +21,31 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    fbLoginSuccess: function (userData) {
+        console.log(userData.authResponse.userID);
+        $.ajax({
+            type: "POST",
+            url: "https://jorenvh.webhosting.be/api/login",
+            dataType: "json",
+            data: userData.authResponse.userID,
+            success: function (success_response) {
+
+            },
+            error: function (error_response) {
+
+            }
+        });
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onDeviceReady: function() {
+        facebookConnectPlugin.login(["public_profile"],
+            app.fbLoginSuccess,
+            function (error) {
+                alert("" + error)
+            }
+        );
     }
 };
 
