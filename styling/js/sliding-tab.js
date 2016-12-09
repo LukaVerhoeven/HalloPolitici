d4f3ab942a356d99c530428d6915e5fddf4951d4
LukaVerhoeven <lka.v.lv@gmail.com>
2016-12-09 15:28:16 +0100

    step-1

--- styling/js/sliding-tab.js
+++ styling/js/sliding-tab.js
@@ -4,5 +4,16 @@ $(".nav--item").click(function(){
 	$(this).addClass('-is--active');
 
 	$(".tabset--content").attr('data-active-tab', val);
+  var navbar = $(".tabset--nav");
+  if (val === "tab-1") {
+    navbar.css("background-image","url(img/navbar1.png)");
+  }else if (val === "tab-2") {
+    $(".tabset--nav").css("background-image","url(img/navbar2.png)");
+  }else if (val === "tab-3") {
+    navbar.css("background-image","url(img/navbar3.png)");
+  }else {
+    navbar.css("background-image","url(img/navbar4.png)");
+  }
+
 
 });
