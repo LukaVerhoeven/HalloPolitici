23d5648e5634a28205bf4ffd183deaaa7d936241
LukaVerhoeven <lka.v.lv@gmail.com>
2016-12-09 16:12:55 +0100

    stepCounter

--- styling/js/sliding-tab.js
+++ styling/js/sliding-tab.js
@@ -5,14 +5,19 @@ $(".nav--item").click(function(){
 
 	$(".tabset--content").attr('data-active-tab', val);
   var navbar = $(".tabset--nav");
+  var step = $('.step');
   if (val === "tab-1") {
     navbar.css("background-image","url(img/navbar1.png)");
+    step.text("Step 1");
   }else if (val === "tab-2") {
-    $(".tabset--nav").css("background-image","url(img/navbar2.png)");
+    navbar.css("background-image","url(img/navbar2.png)");
+    step.text("Step 2");
   }else if (val === "tab-3") {
     navbar.css("background-image","url(img/navbar3.png)");
+    step.text("Step 3");
   }else {
     navbar.css("background-image","url(img/navbar4.png)");
+    step.text("Step 4");
   }
 
 
