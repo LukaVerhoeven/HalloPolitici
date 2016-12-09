00c344171fc4810cd1db4fb20be6a6913adee503
DriesH <dries.heyninck@gmail.com>
2016-12-09 10:20:31 +0100

    new phonegap project

--- MobileAPP/hallopolitici/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('defaulr', function() {
-    gulp.src('./src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./www/css/'));
-});
