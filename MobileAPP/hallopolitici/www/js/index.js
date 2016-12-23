6a3b99e8b480dddff1d596b56ed1e0cf36998f1f
Dries Heyninck <dries.heyninck@gmail.com>
2016-12-23 14:22:41 +0100

    checked if logged in

--- MobileAPP/hallopolitici/www/js/index.js
+++ MobileAPP/hallopolitici/www/js/index.js
@@ -1,86 +1,97 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 
 // Global variables
 var _USERID;
 var _USERNAME;
 var _LOGINBTN;
+var _ISLOGGEDIN = 0;
 var _APILINK = "http://jorenvh.webhosting.be/api";
 
 // Application module
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.getButtons();
-        //this.faceBookLogin();
-        this.bindEvents();
-    },
-    getButtons: function () {
-        // Get login button
-        try {
-            _LOGINBTN = document.querySelector("#loginFB");
-        }
-        catch {
-            _LOGINBTN = "no button";
-        }
+        // Application Constructor
+        initialize: function() {
+            this.getButtons();
+            this.bindEvents();
+        },
+        getButtons: function () {
+            // Get login button
+            try {
+                _LOGINBTN = document.querySelector("#loginFB");
+            }
+            catch (error){
 
-    },
-    bindEvents: function() {
-        // Bind device ready event to app
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        // Bind click event to login button
-        _LOGINBTN.addEventListener('click', function () {
-            app.faceBookLogin();
-        });
-    },
-    fbLoginSuccess: function (userData) {
-        // Debug the userID if it exists and get user name
-        if(userData.authResponse) {
-            // Get user id from the JSON response
-            _USERID = userData.authResponse.userID;
-            // Get user name from facebook id
-            facebookConnectPlugin.api('/me', null, app.getFbUserName);
-        }
-        else{
-            alert('error happend while logging in')
-        }
+            }
+        },
+        bindEvents: function() {
+            // Bind device ready event to app
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+            // Bind click event to login button
+            if(_LOGINBTN !== null) {
+                _LOGINBTN.addEventListener('click', function () {
+                    app.faceBookLogin();
+                });
+            }
 
-        // Check if userid is set local and send user id to back-end
-        if((_USERID !== null && _USERNAME !== null) || (_USERID !== undefined && _USERNAME !== undefined)) {
-            app.sendFbIdAndName(); // Ajax call to back-end with userID and username that are stored in global _USERID _USERNAME
-        }
+        },
+        checkIfLoggedIn: function () {
+            // check if logged in, in global variable
+            if (localStorage.getItem(2) === "1" && (window.location.pathname === "index.html" || window.location.pathname === "/")) {
+                window.location = "steps.html";
+            }
+        },
+        fbLoginSuccess: function (userData) {
+            // Debug the userID if it exists and get user name
+            if(userData.authResponse) {
+                // Get user id from the JSON response
+                _USERID = userData.authResponse.userID;
+                _ISLOGGEDIN = 1;
+                // Get user name from facebook id
+                facebookConnectPlugin.api('/me', null, app.getFbUserName);
+            }
+            else{
+                alert('error happend while logging in')
+            }
 
-        // Set user id in local storage
-        var userID = window.localStorage.key(0);
-        window.localStorage.setItem(0, _USERID);
-    },
-    getFbUserName: function (response) {
-        _USERNAME = response.name; // Put username in global _USERNAME
+            // Check if userid is set local and send user id to back-end
+            if((_USERID !== null && _USERNAME !== null) || (_USERID !== undefined && _USERNAME !== undefined)) {
+                app.sendFbIdAndName(); // Ajax call to back-end with userID and username that are stored in global _USERID _USERNAME
+            }
 
-        var username = window.localStorage.key(1);
-        window.localStorage.setItem(1, _USERNAME);
+            // Set user id in local storage
+            //var userID = window.localStorage.key(0);
+            window.localStorage.setItem(0, _USERID);
+            //var isLoggedIn = window.localStorage.key(2);
+            window.localStorage.setItem(2, _ISLOGGEDIN);
+        },
+        getFbUserName: function (response) {
+            _USERNAME = response.name; // Put username in global _USERNAME
 
-    },
-    faceBookLogin: function() {
-        // Facebook API call for login
-        facebookConnectPlugin.login(["public_profile"],
+            var username = window.localStorage.key(1);
+            window.localStorage.setItem(1, _USERNAME);
+
+        },
+        faceBookLogin: function() {
+            // Facebook API call for login
+            facebookConnectPlugin.login(["public_profile"],
             app.fbLoginSuccess,
             function (error_response) {
                 alert("" + error_response)
@@ -111,6 +122,11 @@ var app = {
     onDeviceReady: function() {
         // Debug device ready event in console
         console.log('device is ready');
+        // check if looge
+        if(window.location.pathname === "/index.html" || window.location.pathname === "/") {
+            app.checkIfLoggedIn();
+        }
+
     },
     getAllPoliticians: function (userID) {
         $.ajax({
