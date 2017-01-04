fd9c2afbeec40964de2f05f6c5762f2d9709434b
LukaVerhoeven <lka.v.lv@gmail.com>
2017-01-04 14:03:25 +0100

    swiping texetbalon

--- styling/js/sliding-tab.js
+++ styling/js/sliding-tab.js
@@ -36,12 +36,36 @@ $(".fullvraag").click(function(){
 //textbalon stap4
 $(function(){
     var swiper = document.getElementById("center");
+		var items = $("#center").children().length;
+		var current = 0;
+		var itemwidth = $("#center").children().width();
+		$(swiper).css('width',itemwidth * items +"vw");
+		var hidding = $(swiper).children(".canvastextbalon:not(:first-child)").css('opacity',0);
 
-    Hammer(swiper).on("swipeleft", function() {
-          $(swiper).animate({left: "-=80vw"}, 500)
-    });
+
+
+			Hammer(swiper).on("swipeleft", function() {
+					if (current <items-1) {
+						var currentBalon = swiper.children[current];
+						$(currentBalon).animate({opacity: 0}, 500)
+						current++;
+						var nextBalon = swiper.children[current];
+						$(nextBalon).animate({opacity: 1}, 500)
+						$(swiper).animate({left: "-=80vw"}, 500);
+
+					}
+			});
+
+
 
     Hammer(document.getElementById("center")).on("swiperight", function() {
+				if (current >0) {
+					var currentBalon = swiper.children[current];
+					$(currentBalon).animate({opacity: 0}, 500)
+					current--;
+					var nextBalon = swiper.children[current];
+					$(nextBalon).animate({opacity: 1}, 500)
           $(swiper).animate({left: "+=80vw"}, 500)
+				}
     });
 })
