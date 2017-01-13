b1a05343c4ee67a3b79ed455c5766434ae693f24
jorenvh1 <joren.vh@hotmail.com>
2017-01-13 17:07:43 +0100

    frger

--- MobileAPP/hallopolitici/www/js/sendFunny.js
+++ MobileAPP/hallopolitici/www/js/sendFunny.js
@@ -1,32 +1,79 @@
 var _HOSTING = "http://jorenvh.webhosting.be";
+var _UNIQUE_IMG_NAME = "";
 var counter = 0;
 
 const funny = {
-sendFunnyElement: function (text,imgURL) {
-  $.ajax({
-        type: "POST",
-        url: _HOSTING + "/api" + "/generate-img",
-        crossDomain: true,
-        data: {
-            "text": text,
-            "image_path": imgURL
-        },
-        success: function (success_response) {
-            console.log(_HOSTING + "/image/" +success_response);
-        },
-        error: function (error_response) {
-            console.log(error_response);
+    shareFunnyOnFacebook: function (text,imgURL) {
+        if (counter < 1 || _UNIQUE_IMG_NAME == "") {
+            $.ajax({
+                type: "POST",
+                url: _HOSTING + "/api/generate-img",
+                crossDomain: true,
+                data: {
+                    "text": text,
+                    "image_path": imgURL
+                },
+                success: function (success_response) {
+                    _UNIQUE_IMG_NAME = success_response;
+                    console.log(_HOSTING + "/image/" +success_response);
+                    //shareeeeeeeeeeeeeee
+                },
+                error: function (error_response) {
+                    console.log(error_response);
+                }
+            });
         }
-    });
-  }
+        else {
+            //shareeeee
+        }
+    },
+    downloadFunny: function(text,imgURL) {
+        if (counter < 1  || _UNIQUE_IMG_NAME == "") {
+            $.ajax({
+                type: "POST",
+                url: _HOSTING + "/api/generate-img",
+                crossDomain: true,
+                data: {
+                    "text": text,
+                    "image_path": imgURL
+                },
+                success: function (success_response) {
+                    _UNIQUE_IMG_NAME = success_response;
+                    console.log(_HOSTING + "/image/" +success_response);
+                    var anchor = document.createElement('a');
+                    anchor.href = _HOSTING + "/image/" + _UNIQUE_IMG_NAME;
+                    anchor.target = '_blank';
+                    anchor.download = _HOSTING + "/image/" + _UNIQUE_IMG_NAME + '.png';
+                    anchor.click();
+                },
+                error: function (error_response) {
+                    console.log(error_response);
+                }
+            });
+        }
+        else {
+            var anchor = document.createElement('a');
+            anchor.href = _HOSTING + "/image/" + _UNIQUE_IMG_NAME;
+            anchor.target = '_blank';
+            anchor.download = _HOSTING + "/image/" + _UNIQUE_IMG_NAME + '.png';
+            anchor.click();
+        }
+    }
 }
 
 
 $('#FacebookPost').click(function () {
-  if (counter <2) {
-    counter++;
-    var imgURL = $('.canvaspic').attr('src');
-    var text = $('.activeBalon').text();
-    funny.sendFunnyElement(text,imgURL);  }
+        counter++;
+        var imgURL = $('.canvaspic').attr('src');
+        var text = $('.activeBalon').text();
+        funny.shareFunnyOnFacebook(text,imgURL);
+
+});
+
+$('#download').click(function () {
+        counter++;
+        var imgURL = $('.canvaspic').attr('src');
+        var text = $('.activeBalon').text();
+        funny.downloadFunny(text,imgURL);
 
 });
